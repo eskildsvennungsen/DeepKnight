8e48d29b54ccd54312a6a474850e6b24b9101a10
Eskild Svennungsen <eskildsvennungsen@gmail.com>
2024-11-05 21:00:12 +0100

    new files

---
+++ src/Tables.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Eskild on 05.11.2024.
+//
+
+#include "Tables.h"
---
+++ src/Tables.h
@@ -0,0 +1,16 @@
+//
+// Created by Eskild on 05.11.2024.
+//
+
+#ifndef TABLES_H
+#define TABLES_H
+
+
+
+class Tables {
+
+};
+
+
+
+#endif //TABLES_H
